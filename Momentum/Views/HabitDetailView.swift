dfcf5b0eacc56dc2c9506a7caa702fe188a2510d
twkim-build <257763068+twkim-build@users.noreply.github.com>
2026-03-31 17:04:57 +0200

    #1 Create project
    Resolves #1

---
+++ Momentum/Views/HabitDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  HabitDetailView.swift
+//  Momentum
+//
+//  Created by taewoo kim on 31.03.26.
+//
+
+import SwiftUI
+
+struct HabitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitDetailView()
+}
---
+++ Momentum/Views/HabitListView.swift
@@ -0,0 +1,18 @@
+//
+//  HabitListView.swift
+//  Momentum
+//
+//  Created by taewoo kim on 31.03.26.
+//
+
+import SwiftUI
+
+struct HabitListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitListView()
+}
